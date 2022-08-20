7ae3880b7e705eac40b7aca054176e660843e1e7
Rohit Roy <rhtry.tech@gmail.com>
2022-08-20 22:20:15 +0530

    Time: 26 ms (11.91%), Space: 6 MB (39.80%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1){ 
+        return n;
+        }
+        else{ 
+        return fib(n-1) + fib(n-2);
+        }
+    }
+        
+        int main(){ 
+        int n; 
+        cin >> n; 
+        cout << fib(n); 
+        return 0;
+        }
+};

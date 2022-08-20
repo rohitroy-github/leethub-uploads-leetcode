34487084c8db003d93f7ecc35314ce5c44e32587
Rohit Roy <rhtry.tech@gmail.com>
2022-08-20 22:19:11 +0530

    Time: 0 ms (100.00%), Space: 6 MB (52.85%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int tribonacci(int n) { 
+        int arr[n+1]; 
+        int i;
+        
+        arr[0] = 0; 
+        if(n >= 1){ 
+        arr[1] = 1;}
+        if(n >= 2){ 
+        arr[2] = 1;}
+        
+        for(i=3;i<=n;i++)
+        { 
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        }
+        
+        return arr[n];
+    }
+};

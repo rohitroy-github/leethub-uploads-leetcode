13c3f2536aac8e0679cd1ff6feb620063d7c15ee
Rohit Roy <rhtry.tech@gmail.com>
2022-08-20 22:36:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.79%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     
-    //Recursion Solution 
+    /**Recursion Solution 
     int fib(int N){ 
     if(N < 2){ 
     return N;}
     else{ 
     return fib(N-1) + fib(N-2);}
     }
+    **/
+    
+    //DP Space Optimized 
+    int fib(int N) {
+        if (N == 0) return 0;
+        int a = 0, b = 1;
+        for(int i=2; i<=N; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        } return b;  
+    }  
 };

3e0ca917ab8ade55253d606d82aab85e77dbf07d
Rohit Roy <rhtry.tech@gmail.com>
2022-08-23 23:06:22 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (94.50%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,22 @@
+unordered_map<char, string> L({{'2',"abc"},{'3',"def"},{'4',"ghi"},
+    {'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}});
+
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-                static const vector<string> lookup = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        
-        if (empty(digits)) {
-            return {};
+    vector<string> letterCombinations(string D) {
+        int len = D.size();
+        vector<string> ans;
+        if (!len) return ans;
+        bfs(0, len, "", ans, D);
+        return ans;
+    }
+
+    void bfs(int pos, int &len, string str, vector<string> &ans, string &D) {
+        if (pos == len) ans.push_back(str);
+        else {
+            string letters = L[D[pos]];
+            for (int i = 0; i < letters.size(); i++)
+                bfs(pos+1, len, str+letters[i], ans, D);
         }
-        int total = 1;
-        for (const auto& digit : digits) {
-            total *= size(lookup[digit - '0']);
-        }
-        vector<string> result;
-        for (int i = 0; i < total; ++i) {
-            int base = total;
-            string curr;
-            for (const auto& digit : digits) {
-                const auto& choices = lookup[digit - '0'];
-                base /= size(choices);
-                curr.push_back(choices[(i / base) % size(choices)]);
-            }
-            result.emplace_back(move(curr));
-        }
-        return result;
     }
 };
